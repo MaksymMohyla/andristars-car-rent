083bbfee39caddfe3e1e893a5fdf55342870dbad
MaksymMohyla <greenparksquad@gmail.com>
2025-04-14 13:13:24 +0300

    optimized build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,37 @@
-const { src, dest, watch, series } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
+const clean = require('gulp-clean');
+
+const buildDir = './dist';
 
 function buildStyles() {
-  return src('./src/styles/**/*.scss').pipe(sass()).pipe(dest('./src/styles'));
+  return src('./src/styles/**/*.scss')
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(dest(`${buildDir}/styles`));
+}
+
+function copyHTML() {
+  return src('./src/*.html').pipe(dest(buildDir));
+}
+
+function copyJS() {
+  return src('./src/scripts/**/*.js')
+    .pipe(uglify())
+    .pipe(dest(`${buildDir}/scripts`));
+}
+
+function cleanDist() {
+  return src(buildDir, { read: false, allowEmpty: true }).pipe(clean());
 }
 
 function watchTask() {
   watch(['./src/styles/**/*.scss'], buildStyles);
+  watch(['./src/*.html'], copyHTML);
+  watch(['./src/scripts/**/*.js'], copyJS);
 }
 
-exports.default = series(buildStyles, watchTask);
+exports.build = series(cleanDist, parallel(buildStyles, copyHTML, copyJS));
+exports.default = series(exports.build, watchTask);
